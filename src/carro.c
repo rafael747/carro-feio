6ad013252e4069e51b071c5a1183e61552106b8f
Rafael Carreira <rafael_carreira11@hotmail.com>
2016-11-30 17:21:33 -0200

    primeiro commit com o codigo inicial feio

---
+++ src/carro.c
@@ -0,0 +1,436 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+#include <stdio.h>
+#include <GL/freeglut.h>
+
+//Declaração de Variáveis Globais
+//int projecao=0; //Variável Lógica para Definir o Tipo de Projeção (Perspectiva ou Ortogonal)
+int posx=0, posy=50, posz=-80; //Variáveis que definem a posição da câmera
+int oy=0,ox=0,oz=0;         //Variáveis que definem para onde a câmera olha
+int lx=0, ly=1,  lz=0;         //Variáveis que definem qual eixo estará na vertical do monitor.
+
+float limiteEsq=-15, limiteDir=15;
+float car=-45,rua=0;
+//Protótipos das Funções
+void Inicializa();
+void Display();
+void Mouse(int botao, int estado, int x, int y);
+void keyboard (unsigned char key, int x, int y);
+void TeclasEspeciais (int key, int x, int y);
+	
+
+void ruaReta()
+{ 
+	glPushMatrix();
+	glTranslatef(0,.2,rua--);
+	glColor3ub(50,50,50); //rua
+	glBegin(GL_QUADS);
+	glVertex3f(limiteEsq, .2, 0);
+	glVertex3f(limiteDir, .2, 0);
+	glVertex3f(limiteDir, .2, 500);
+	glVertex3f(limiteEsq, .2, 500);
+	glEnd();
+        glPopMatrix();
+}
+
+
+void ruaCurva()
+{
+   int i;
+   for(i=0; i<500; i++)
+   {
+	glPushMatrix();
+	glTranslatef(0,.2,rua--);
+	glColor3ub(50,50,50);
+	glBegin(GL_QUADS);
+	glVertex3f(limiteEsq-i, .2, i);
+	glVertex3f(limiteDir-i, .2, i);
+	glVertex3f(limiteDir-i, .2, i+1);
+	glVertex3f(limiteEsq-i, .2, i+1);
+	glEnd();
+        glPopMatrix();
+   }
+}
+
+void Display()
+{
+   
+   // Inicializa parâmetros de rendering
+    // Define a cor de fundo da janela de visualização como preta
+   glClearColor(0.2, 0.2, 0.5, 0.0);
+
+   glEnable(GL_DEPTH_TEST);
+   glEnable(GL_LINE_SMOOTH);
+   glEnable(GL_POLYGON_SMOOTH);
+
+
+   glEnable(GL_SMOOTH);
+   glEnable(GL_BLEND);
+
+   glMatrixMode(GL_PROJECTION);/*glMatrixMode()- define qual matriz será alterada. SEMPRE defina o tipo de apresentação 
+                              (Ortogonal ou Perspectiva) na matriz PROJECTION.*/
+   glLoadIdentity();//"Limpa" ou "transforma" a matriz em identidade, reduzindo possíveis erros.
+
+   //if (projecao==1)
+   //   glOrtho(-150, 150, -150, 150, -150, 150); //Define a projeção como ortogonal
+   //else
+      gluPerspective(45,1,1,1000); //Define a projeção como perspectiva
+   
+   glMatrixMode(GL_MODELVIEW);/*glMatrixMode()- define qual matriz será alterada. SEMPRE defina a câmera 
+                              (Ortogonal ou Perspectiva) na matriz MODELVIEW (onde o desenho ocorrerá).*/
+   glLoadIdentity(); ////"Limpa" ou "transforma" a matriz em identidade, reduzindo possíveis erros.
+
+   gluLookAt(posx,posy,posz,ox,oy,oz,lx,ly,lz); //Define a pos da câmera, para onde olha e qual eixo está na vertical.
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); /* "limpa" um buffer particular ou combinações de buffers, 
+                                                         onde buffer é uma área de armazenamento para informações da imagem. 
+                                                        Nesse caso, está "limpando os buffers para suportarem animações */
+   //Chamada para Função  ou funções para desenhar o objeto/cena...
+   //----------------------------------------------------------------
+   //glPushMatrix(); //Salva o estado atual da cena. O que for desenhado após o Push não influenciará o já representado
+
+
+   int x,y,z;
+
+      glColor3ub(70, 100, 40); //terreno verde
+	glBegin(GL_QUADS);
+	glVertex3f(-500, 0, -50);
+	glVertex3f(500, 0, -50);
+	glVertex3f(500, 0, 500);
+	glVertex3f(-500, 0, 500);
+	glEnd();
+ 
+
+// treixo de rua reta
+
+	ruaReta();
+	//ruaCurva();
+
+
+
+    glColor3ub(217,217,25); // rua marcas
+	for( z=5000; z>-10; z=z-10)
+	{
+		glPushMatrix();
+		glTranslatef(0,.3,rua);
+
+		glBegin(GL_QUADS);
+		glVertex3f(-1, 0.3, z-6);
+		glVertex3f(-1, 0.3, z);
+		glVertex3f(1, 0.3, z);
+		glVertex3f(1, 0.3, z-6);
+		glEnd();
+        	
+		glPopMatrix();
+		
+	}
+
+    /*  glColor3ub(56,176,222); //piscina legal
+	glBegin(GL_QUADS);
+	glVertex3f(-15, 0.1, 15);
+	glVertex3f(10, 0.1, 15);
+	glVertex3f(10, 0.1, 35);
+	glVertex3f(-15, 0.1, 35);
+	glEnd();
+*/
+
+// cerca dahora
+
+     	glColor3ub(235,199,158); // cerca
+	for( z=-50; z<200; z=z+3)
+	{
+		glBegin(GL_QUADS);
+		glVertex3f(-35, 0, z);
+		glVertex3f(-35, 6, z);
+		glVertex3f(-35, 6, z+2);
+		glVertex3f(-35, 0, z+2);
+		glEnd();
+		
+		glBegin(GL_QUADS); //divisoria
+		glVertex3f(-35, 3, z+2);
+		glVertex3f(-35, 4, z+2);
+		glVertex3f(-35, 4, z+3);
+		glVertex3f(-35, 3, z+3);
+		glEnd();
+		
+	}
+
+
+/*      glColor3ub(216,216,191); //fundacao casa cinza
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 0.2, -10);
+	glVertex3f(20, 0.2, -10);
+	glVertex3f(20, 0.2, 10);
+	glVertex3f(-20, 0.2, 10);
+	glEnd();
+ 
+    glColor3ub(255,28,174); // chaleira rosa
+    glutWireTeapot(2.0);
+    
+      glColor3ub(165,128,100); // parede esquerda
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 0, -10);
+	glVertex3f(-20, 0, 10);
+	glVertex3f(-20, 10, 10);
+	glVertex3f(-20, 10, -10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede direita
+	glBegin(GL_QUADS);
+	glVertex3f(20, 0, -10);
+	glVertex3f(20, 0, 10);
+	glVertex3f(20, 10, 10);
+	glVertex3f(20, 10, -10);
+	glEnd();
+
+     //glColor3ub(112,147,219); // parede frente A
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 0, -10);
+	glVertex3f(-20, 8, -10);
+	glVertex3f(10, 8, -10);
+	glVertex3f(10, 0, -10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede frente B
+	glBegin(GL_QUADS);
+	glVertex3f(15, 0, -10);
+	glVertex3f(15, 8, -10);
+	glVertex3f(20, 8, -10);
+	glVertex3f(20, 0, -10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede frente C
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 8, -10);
+	glVertex3f(-20, 10, -10);
+	glVertex3f(20, 10, -10);
+	glVertex3f(20, 8, -10);
+	glEnd();
+
+
+     //glColor3ub(168,168,168); // parede fundo A
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 0, 10);
+	glVertex3f(-20, 3, 10);
+	glVertex3f(20, 3, 10);
+	glVertex3f(20, 0, 10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede fundo B
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 7, 10);
+	glVertex3f(-20, 10, 10);
+	glVertex3f(20, 10, 10);
+	glVertex3f(20, 7, 10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede fundo C
+	glBegin(GL_QUADS);
+	glVertex3f(-20, 3, 10);
+	glVertex3f(-20, 7, 10);
+	glVertex3f(-10, 7, 10);
+	glVertex3f(-10, 3, 10);
+	glEnd();
+
+     //glColor3ub(168,168,168); // parede fundo C
+	glBegin(GL_QUADS);
+	glVertex3f(-5, 3, 10);
+	glVertex3f(-5, 7, 10);
+	glVertex3f(20, 7, 10);
+	glVertex3f(20, 3, 10);
+	glEnd();
+*/	
+	
+	glPushMatrix();
+        glColor3ub(23,234,23); //carro
+        glTranslatef(car,3,-30);
+        glScalef(8, 3, 6);
+        glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix(); //roda 1
+	glColor3ub(0,0,0);
+	glTranslatef(car-3,2,-34);
+	glScalef(3, 3, 8);
+        glutSolidCylinder(.5,1,50,50);	
+        glPopMatrix();
+
+	glPushMatrix(); //roda 2
+	glColor3ub(0,0,0);
+	glTranslatef(car+3,2,-34);
+	glScalef(3, 3, 8);
+        glutSolidCylinder(.5,1,50,50);	
+        glPopMatrix();
+	
+	//glPushMatrix();
+        //glColor3ub(220,220,220); //cubo
+        //glTranslatef(10,3,-30);
+        //glScalef(8, 8, 8);
+        //glutSolidCube(1);
+	//glPopMatrix();
+
+	glPushMatrix();
+        glColor3ub(200,5,70); //carro 2
+        glTranslatef(10,3,-30);
+        glScalef(8, 3, 6);
+        glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix(); //roda 1
+	glColor3ub(0,0,0);
+	glTranslatef(7,2,-34);
+	glScalef(3, 3, 8);
+        glutSolidCylinder(.5,1,50,50);	
+        glPopMatrix();
+
+	glPushMatrix(); //roda 2
+	glColor3ub(0,0,0);
+	glTranslatef(13,2,-34);
+	glScalef(3, 3, 8);
+        glutSolidCylinder(.5,1,50,50);	
+        glPopMatrix();
+
+	if(car >= 1)
+		{}//puts("bateu");
+	else
+		car+=.4;
+      
+    
+         // glPopMatrix();  //Retorna ao estado anterior da cena. O que for desenhado após o Push não influenciou o já representado
+   //----------------------------------------------------------------   
+   
+   
+   glutSwapBuffers(); //Executa a Cena. SwapBuffers dá suporte para mais de um buffer, permitindo execução de animações sem cintilações. 
+
+        glutPostRedisplay();
+}
+
+void Mouse(int botao, int estado, int x, int y)
+{  //botão - recebe o código do botão pressionado
+   //estado - recebe se está pressionado ou não
+   //x, y - recebem respectivamente as posições do mouse na tela
+   switch (botao)
+   {
+      case GLUT_LEFT_BUTTON:
+      if (estado == GLUT_DOWN)
+      {
+	      
+      
+      printf("pos:  x: %d  y: %d  z: %d\nolho:  x: %d  y: %d  z: %d\n",posx,posy,posz,ox,oy,oz);
+
+		      
+         //projecao=1;
+         //posx=0; posy=10; posz=20;
+         //ox=0,oy=0,oz=0;
+         //lx=0, ly=1,lz=0;
+        glutPostRedisplay();
+      }
+      break;
+
+      case GLUT_RIGHT_BUTTON:
+      if(estado == GLUT_DOWN)
+      {
+         //projecao=0;
+         //posx=0; posy=10; posz=20;
+         //oy=0; ox=0;  oz=0;
+         //lx=0, ly=1, lz=0;
+         //glutPostRedisplay();
+      }
+      break;
+   }
+}
+
+void keyboard (unsigned char key, int x, int y)
+{//Key - recebe o código ASCII da tecla
+ //x, y - recebem as posições do mouse na tela (permite tratar os dois dispositivos)
+      if (key=='a')
+      {
+	 posx-=3;ox-=3;
+         //lz=0; ly=1; ox+=5;
+      }
+      else if (key=='s')
+      {
+	 posz+=3;oz+=3;
+         //lz=0; ly=1; ox-=5;
+      }
+      else if (key=='d')
+      {
+	 posx+=3;ox+=3;
+         //lz=0; ly=1; oy+=5;
+      }
+      else if (key=='w')    
+      {
+	 posz-=3;oz-=3;
+      }
+      else if (key==' ')
+      {
+	      car-=5;
+              }
+      else if (key==27)
+      {         
+         exit(0);
+      }
+      printf("pos:  x: %d  y: %d  z: %d\nolho:  x: %d  y: %d  z: %d\n",posx,posy,posz,ox,oy,oz);
+      glutPostRedisplay();
+}
+void TeclasEspeciais (int key, int x, int y)
+{//Key - recebe o código ASCII da tecla
+ //x, y - recebem respectivamente as posições mouse na tela (permite tratar os dois dispositivos)
+      if (key==GLUT_KEY_RIGHT)
+      {
+	 ox+=3;
+         //posx+=5; ox+=5;
+      }
+      else if (key==GLUT_KEY_PAGE_UP)
+      {
+         posy+=3;
+         //posy+=5; oy+=5;
+      }
+      else if (key==GLUT_KEY_UP)
+      {
+	 oz+=3;
+	 //oy+=3;
+         //posz-=5; oz-=5;
+      }
+      else if (key==GLUT_KEY_LEFT)    
+      { 
+	 ox-=3;
+         //posx-=5; ox-=5;
+      }
+      else if (key==GLUT_KEY_PAGE_DOWN)
+      {
+	 posy-=3;
+         //posy-=5; oy-=5;
+      }
+      else if (key==GLUT_KEY_DOWN)
+      {    
+	 oz-=3;
+	 //oy-=3;  
+         //posz+=5; oz+=5;
+      }
+      
+      printf("pos:  x: %d  y: %d  z: %d\nolho:  x: %d  y: %d  z: %d\n",posx,posy,posz,ox,oy,oz);
+      
+      glutPostRedisplay();
+}
+int main(int argc,char **argv)
+{
+
+   glutInit(&argc, argv); // Initializes glut
+    
+   
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); /*Define as características do espaço vetorial. 
+                                                                           //  Nesse caso, permite animações (sem cintilações), cores compostas por Verm. Verde e Azul,
+                                                                           //  Buffer que permite trablhar com profundidade e elimina faces escondidas.*/           
+  
+   glutInitWindowSize(1000, 1000);
+   glutInitWindowPosition(400, 400);
+   glutCreateWindow("Estrutura para uma Aplicação 3D");
+   glutDisplayFunc(Display);
+   glutMouseFunc(Mouse);
+   glutKeyboardFunc(keyboard);
+   glutSpecialFunc(TeclasEspeciais);
+   glutMainLoop();
+   return 0; 
+}
